15bf0fb69d85439b948f2a12521504a0879f8400
Eduardo Cabral <eduardosbcabral@gmail.com>
2023-03-03 18:28:55 -0300

    ✨ Refactor to .net 6 and implements JWT authentication

--- QueueConsumer/Program.cs
+++ QueueConsumer/Program.cs
@@ -1,99 +1,107 @@
 using QueueConsumer.Models;
+using QueueConsumer.Notification;
+
 using System;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace QueueConsumer
+namespace QueueConsumer;
+
+public class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main(string[] args = null)
+        ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+        ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+        try
         {
-            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
-            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            var tokenSource = new CancellationTokenSource();
+            var config = QueueConsumerConfiguration.Create();
 
-            try
+            var queueConsumerJwt = new QueueConsumerJwt(config);
+            var sendNotificationClient = new SendNotificationClient(config, queueConsumerJwt);
+
+            DisplayHeader(config);
+
+            var processor = new QueueMessageProcessor(config, sendNotificationClient);
+
+            var task = new Task(() =>
             {
-                var tokenSource = new CancellationTokenSource();
-                var config = QueueConsumerConfiguration.Create();
-                DisplayHeader(config);
+                while (!processor.Execute()) { }
+            }, tokenSource.Token);
 
-                var processor = new QueueMessageProcessor(config);
-
-                var task = new Task(() =>
-                {
-                    while (!processor.Execute()) { }
-                }, tokenSource.Token);
-
-                Console.CancelKeyPress += (sender, eventArgs) =>
-                {
-                    Console.WriteLine("Aborting Program...");
-                    tokenSource.Cancel();
-                    task.Dispose();
-                };
-
-                task.Start();
-                task.Wait();
-            }
-            catch (Exception e)
+            Console.CancelKeyPress += (sender, eventArgs) =>
             {
-                Console.WriteLine("Program Exception:");
-                Console.WriteLine(" - {0}\n\n{1}", e.Message, e.StackTrace);
-            }
+                Console.WriteLine("Aborting Program...");
+                tokenSource.Cancel();
+                task.Dispose();
+            };
+
+            task.Start();
+            task.Wait();
         }
-
-        private static void DisplayHeader(QueueConsumerConfiguration config)
+        catch (Exception e)
         {
-            Logger.LogLineWithLevel("INFO", "Queue Consumer Application Started");
-            Logger.LogLine("");
-            Logger.LogLine("Configuration:");
-            Logger.LogLine("- QueueConnectionString: {0}", Regex.Replace(config.QueueConnectionString, "(\\:\\/\\/).*(\\@)", "://*****@"));
-            Logger.LogLine("- QueueName: {0}", config.QueueName);
-            Logger.LogLine("- Url: {0}", config.Url);
-            Logger.LogLine("- User: {0}", config.User);
-            Logger.LogLine("- Pass: {0}", string.IsNullOrWhiteSpace(config.Pass) ? "null" : "******");
-            Logger.LogLine("- AuthToken: {0}", string.IsNullOrWhiteSpace(config.AuthToken) ? "null" : "******");
-            Logger.LogLine("- TimeoutInSeconds: {0}", config.TimeoutInSeconds);
-            Logger.LogLine("- MaxThreads: {0}", config.MaxThreads);
-            Logger.LogLine("- PopulateQueueQuantity: {0}", config.PopulateQueueQuantity);
-            Logger.LogLine("- CreateQueue: {0}", config.CreateQueue);
-            Logger.LogLine("- RetryCount: {0}", config.RetryCount);
-            Logger.LogLine("- RetryTTL: {0}", config.RetryTTL);
-            Logger.LogLine("- Condition: {0}", config.Condition);
-            Logger.LogLine("- StatusCodeAcceptToSuccessList: {0}", string.Join(";",config.StatusCodeAcceptToSuccessList));
-            Logger.LogLine("");
+            Console.WriteLine("Program Exception:");
+            Console.WriteLine(" - {0}\n\n{1}", e.Message, e.StackTrace);
         }
     }
 
-    public static class Logger
+    private static void DisplayHeader(QueueConsumerConfiguration config)
     {
-        public static void LogLineWithLevel(string logLevel, string message, params object[] args)
-        {
-            var finalMessage = $"[{GetCurrentDate()}][{logLevel}] {message ?? ""}";
-            Console.WriteLine(finalMessage, args);
-        }
+        Logger.LogLineWithLevel("INFO", "Queue Consumer Application Started");
+        Logger.LogLine("");
+        Logger.LogLine("Configuration:");
+        Logger.LogLine("- QueueConnectionString: {0}", Regex.Replace(config.QueueConnectionString, "(\\:\\/\\/).*(\\@)", "://*****@"));
+        Logger.LogLine("- QueueName: {0}", config.QueueName);
+        Logger.LogLine("- Url: {0}", config.Url);
+        Logger.LogLine("- User: {0}", config.User);
+        Logger.LogLine("- Pass: {0}", string.IsNullOrWhiteSpace(config.Pass) ? "null" : "******");
+        Logger.LogLine("- AuthToken: {0}", string.IsNullOrWhiteSpace(config.AuthToken) ? "null" : "******");
+        Logger.LogLine("- AuthUrl: {0}", string.IsNullOrWhiteSpace(config.AuthUrl));
+        Logger.LogLine("- ClientId: {0}", string.IsNullOrWhiteSpace(config.ClientId) ? "null" : "******");
+        Logger.LogLine("- ClientSecret: {0}", string.IsNullOrWhiteSpace(config.ClientSecret) ? "null" : "******");
+        Logger.LogLine("- TimeoutInSeconds: {0}", config.TimeoutInSeconds);
+        Logger.LogLine("- MaxThreads: {0}", config.MaxThreads);
+        Logger.LogLine("- PopulateQueueQuantity: {0}", config.PopulateQueueQuantity);
+        Logger.LogLine("- CreateQueue: {0}", config.CreateQueue);
+        Logger.LogLine("- RetryCount: {0}", config.RetryCount);
+        Logger.LogLine("- RetryTTL: {0}", config.RetryTTL);
+        Logger.LogLine("- Condition: {0}", config.Condition);
+        Logger.LogLine("- StatusCodeAcceptToSuccessList: {0}", string.Join(";",config.StatusCodeAcceptToSuccessList));
+        Logger.LogLine("");
+    }
+}
 
-        public static void LogWithLevel(string logLevel, string message, params object[] args)
-        {
-            var finalMessage = $"[{GetCurrentDate()}][{logLevel}] {message ?? ""}";
-            Console.Write(finalMessage, args);
-        }
+public static class Logger
+{
+    public static void LogLineWithLevel(string logLevel, string message, params object[] args)
+    {
+        var finalMessage = $"[{GetCurrentDate()}][{logLevel}] {message ?? ""}";
+        Console.WriteLine(finalMessage, args);
+    }
 
-        public static void LogLine(string message, params object[] args)
-        {
-            Console.WriteLine(message, args);
-        }
+    public static void LogWithLevel(string logLevel, string message, params object[] args)
+    {
+        var finalMessage = $"[{GetCurrentDate()}][{logLevel}] {message ?? ""}";
+        Console.Write(finalMessage, args);
+    }
 
-        public static void Log(string message, params object[] args)
-        {
-            Console.Write(message, args);
-        }
+    public static void LogLine(string message, params object[] args)
+    {
+        Console.WriteLine(message, args);
+    }
 
-        private static string GetCurrentDate()
-        {
-            return DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
-        }
+    public static void Log(string message, params object[] args)
+    {
+        Console.Write(message, args);
+    }
+
+    private static string GetCurrentDate()
+    {
+        return DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
     }
 }
--- QueueConsumer/Queue/QueueManager.cs
+++ QueueConsumer/Queue/QueueManager.cs
@@ -1,170 +1,172 @@
 ﻿using Newtonsoft.Json;
+
 using QueueConsumer.Models;
+
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using RabbitMQ.Client.Framing;
+
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace QueueConsumer.Queue
+namespace QueueConsumer.Queue;
+
+public class QueueManager : IDisposable
 {
-    public class QueueManager : IDisposable
+    private IModel Channel;
+
+    public Action<string, int, ulong> ReceiveMessage;
+
+    public QueueConsumerConfiguration Configuration { get; set; }
+
+    public QueueManager(QueueConsumerConfiguration configuration)
     {
-        private IModel Channel;
+        this.Configuration = configuration;
+    }
 
-        public Action<string, int, ulong> ReceiveMessage;
-
-        public QueueConsumerConfiguration Configuration { get; set; }
-
-        public QueueManager(QueueConsumerConfiguration configuration)
-        {
-            this.Configuration = configuration;
-        }
-
-        public void AddRetryMessage(string message, int retryCount)
-        {
-            var buffer = Encoding.UTF8.GetBytes(message);
-            this.Channel.BasicPublish(
-                exchange: "",
-                routingKey: $"{this.Configuration.QueueName}-retry",
-                basicProperties: new BasicProperties
-                {
-                    Persistent = true,
-                    Headers = new Dictionary<string, object>
-                    {
-                        { "retry_count", retryCount }
-                    }
-                },
-                body: buffer);
-        }
-
-        public void AddDeadMessage(string message)
-        {
-            var buffer = Encoding.UTF8.GetBytes(message);
-            this.Channel.BasicPublish(
-                exchange: "",
-                routingKey: $"{this.Configuration.QueueName}-dead",
-                basicProperties: new BasicProperties { Persistent = true },
-                body: buffer);
-        }
-
-        public void AddMessage(string message)
-        {
-            var buffer = Encoding.UTF8.GetBytes(message);
-            this.Channel.BasicPublish(
-                exchange: "",
-                routingKey: this.Configuration.QueueName,
-                basicProperties: new BasicProperties { Persistent = true },
-                body: buffer);
-        }
-
-        public void Ack(ulong deliveryTag)
-        {
-            Channel.BasicAck(deliveryTag, false);
-        }
-
-        public void NAck(ulong deliveryTag, bool requeued = true)
-        {
-            Channel.BasicNack(deliveryTag, false, requeued);
-        }
-
-        public void TryConnect()
-        {
-            try
+    public void AddRetryMessage(string message, int retryCount)
+    {
+        var buffer = Encoding.UTF8.GetBytes(message);
+        this.Channel.BasicPublish(
+            exchange: "",
+            routingKey: $"{this.Configuration.QueueName}-retry",
+            basicProperties: new BasicProperties
             {
-                ChannelFactory.CloseConnection();
-
-                var connectionFactory = new ConnectionFactory()
+                Persistent = true,
+                Headers = new Dictionary<string, object>
                 {
-                    RequestedHeartbeat = 30,
-                    NetworkRecoveryInterval = TimeSpan.FromSeconds(5),
-                    TopologyRecoveryEnabled = true,
-                    Uri = new Uri(this.Configuration.QueueConnectionString)
-                };
-
-                this.Channel = ChannelFactory.Create(connectionFactory);
-                Logger.LogLineWithLevel("OK", "TryConnect: Successfully connected!");
-
-                this.Configure();
-            }
-            catch (Exception e)
-            {
-                Logger.LogLineWithLevel("ERROR", "TryConnect: An exception occurred");
-                Logger.LogLineWithLevel("ERROR", "Message: {0}", e.Message);
-                throw;
-            }
-        }
-
-        private void Configure()
-        {
-            if (this.Configuration.CreateQueue)
-            {
-                Logger.LogLineWithLevel("OK", "CreateQueue with TTL: {0} ", this.Configuration.RetryTTL);
-                this.Channel.ExchangeDeclare($"{this.Configuration.QueueName}-exchange", "direct", true);
-                this.Channel.QueueDeclare(this.Configuration.QueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
-                this.Channel.QueueBind(this.Configuration.QueueName,
-                                       $"{this.Configuration.QueueName}-exchange",
-                                       $"{this.Configuration.QueueName}-routing-key", null);
-
-                var retryQueueArgs = new Dictionary<string, object>
-                {
-                    { "x-dead-letter-exchange", $"{this.Configuration.QueueName}-exchange"},
-                    { "x-dead-letter-routing-key", $"{this.Configuration.QueueName}-routing-key"},
-                    { "x-message-ttl", this.Configuration.RetryTTL}
-                };
-
-                this.Channel.ExchangeDeclare($"{this.Configuration.QueueName}-retry-exchange", "direct", true);
-                this.Channel.QueueDeclare($"{this.Configuration.QueueName}-retry", true, false, false, retryQueueArgs);
-                this.Channel.QueueBind($"{this.Configuration.QueueName}-retry",
-                                       $"{this.Configuration.QueueName}-retry-exchange",
-                                       $"{this.Configuration.QueueName}-retry-routing-key", null);
-
-                this.Channel.ExchangeDeclare($"{this.Configuration.QueueName}-dead-exchange", "direct", true);
-                this.Channel.QueueDeclare($"{this.Configuration.QueueName}-dead", true, false, false, null);
-                this.Channel.QueueBind($"{this.Configuration.QueueName}-dead",
-                                       $"{this.Configuration.QueueName}-dead-exchange",
-                                       $"{this.Configuration.QueueName}-dead-routing-key", null);
-            }
-
-            if (this.Configuration.PopulateQueueQuantity > 0)
-            {
-                Logger.LogLineWithLevel("OK", "PopulateQueueQuantity: {0} ", this.Configuration.PopulateQueueQuantity);
-                Console.CursorVisible = false;
-                for (int i = 0; i < this.Configuration.PopulateQueueQuantity; i++)
-                {
-                    this.AddMessage(JsonConvert.SerializeObject(new { message = i }));
+                    { "retry_count", retryCount }
                 }
-                Console.CursorVisible = true;
-                Console.WriteLine();
-            }
+            },
+            body: buffer);
+    }
 
-            var consumer = new EventingBasicConsumer(this.Channel);
+    public void AddDeadMessage(string message)
+    {
+        var buffer = Encoding.UTF8.GetBytes(message);
+        this.Channel.BasicPublish(
+            exchange: "",
+            routingKey: $"{this.Configuration.QueueName}-dead",
+            basicProperties: new BasicProperties { Persistent = true },
+            body: buffer);
+    }
 
-            consumer.Received += this.Received;
+    public void AddMessage(string message)
+    {
+        var buffer = Encoding.UTF8.GetBytes(message);
+        this.Channel.BasicPublish(
+            exchange: "",
+            routingKey: this.Configuration.QueueName,
+            basicProperties: new BasicProperties { Persistent = true },
+            body: buffer);
+    }
 
-            Channel.BasicQos(0, (ushort)this.Configuration.MaxThreads, false);
-            Channel.BasicConsume(queue: this.Configuration.QueueName, consumer: consumer);
-        }
+    public void Ack(ulong deliveryTag)
+    {
+        Channel.BasicAck(deliveryTag, false);
+    }
 
-        private void Received(object model, BasicDeliverEventArgs eventArgs)
-        {
-            object headerValue = null;
-            try
-            {
-                eventArgs?.BasicProperties?.Headers?.TryGetValue("retry_count", out headerValue);
-            }
-            catch (Exception) { }
+    public void NAck(ulong deliveryTag, bool requeued = true)
+    {
+        Channel.BasicNack(deliveryTag, false, requeued);
+    }
 
-            int retryCount = headerValue != null ? (int)headerValue : 0;
-
-            var message = Encoding.UTF8.GetString(eventArgs.Body);
-            ReceiveMessage?.Invoke(message, retryCount, eventArgs.DeliveryTag);
-        }
-
-        public void Dispose()
+    public void TryConnect()
+    {
+        try
         {
             ChannelFactory.CloseConnection();
+
+            var connectionFactory = new ConnectionFactory()
+            {
+                RequestedHeartbeat = 30,
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(5),
+                TopologyRecoveryEnabled = true,
+                Uri = new Uri(this.Configuration.QueueConnectionString)
+            };
+
+            this.Channel = ChannelFactory.Create(connectionFactory);
+            Logger.LogLineWithLevel("OK", "TryConnect: Successfully connected!");
+
+            this.Configure();
         }
+        catch (Exception e)
+        {
+            Logger.LogLineWithLevel("ERROR", "TryConnect: An exception occurred");
+            Logger.LogLineWithLevel("ERROR", "Message: {0}", e.Message);
+            throw;
+        }
+    }
+
+    private void Configure()
+    {
+        if (this.Configuration.CreateQueue)
+        {
+            Logger.LogLineWithLevel("OK", "CreateQueue with TTL: {0} ", this.Configuration.RetryTTL);
+            this.Channel.ExchangeDeclare($"{this.Configuration.QueueName}-exchange", "direct", true);
+            this.Channel.QueueDeclare(this.Configuration.QueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            this.Channel.QueueBind(this.Configuration.QueueName,
+                                   $"{this.Configuration.QueueName}-exchange",
+                                   $"{this.Configuration.QueueName}-routing-key", null);
+
+            var retryQueueArgs = new Dictionary<string, object>
+            {
+                { "x-dead-letter-exchange", $"{this.Configuration.QueueName}-exchange"},
+                { "x-dead-letter-routing-key", $"{this.Configuration.QueueName}-routing-key"},
+                { "x-message-ttl", this.Configuration.RetryTTL}
+            };
+
+            this.Channel.ExchangeDeclare($"{this.Configuration.QueueName}-retry-exchange", "direct", true);
+            this.Channel.QueueDeclare($"{this.Configuration.QueueName}-retry", true, false, false, retryQueueArgs);
+            this.Channel.QueueBind($"{this.Configuration.QueueName}-retry",
+                                   $"{this.Configuration.QueueName}-retry-exchange",
+                                   $"{this.Configuration.QueueName}-retry-routing-key", null);
+
+            this.Channel.ExchangeDeclare($"{this.Configuration.QueueName}-dead-exchange", "direct", true);
+            this.Channel.QueueDeclare($"{this.Configuration.QueueName}-dead", true, false, false, null);
+            this.Channel.QueueBind($"{this.Configuration.QueueName}-dead",
+                                   $"{this.Configuration.QueueName}-dead-exchange",
+                                   $"{this.Configuration.QueueName}-dead-routing-key", null);
+        }
+
+        if (this.Configuration.PopulateQueueQuantity > 0)
+        {
+            Logger.LogLineWithLevel("OK", "PopulateQueueQuantity: {0} ", this.Configuration.PopulateQueueQuantity);
+            Console.CursorVisible = false;
+            for (int i = 0; i < this.Configuration.PopulateQueueQuantity; i++)
+            {
+                this.AddMessage(JsonConvert.SerializeObject(new { message = i }));
+            }
+            Console.CursorVisible = true;
+            Console.WriteLine();
+        }
+
+        var consumer = new EventingBasicConsumer(this.Channel);
+
+        consumer.Received += this.Received;
+
+        Channel.BasicQos(0, (ushort)this.Configuration.MaxThreads, false);
+        Channel.BasicConsume(queue: this.Configuration.QueueName, consumer: consumer);
+    }
+
+    private void Received(object model, BasicDeliverEventArgs eventArgs)
+    {
+        object headerValue = null;
+        try
+        {
+            eventArgs?.BasicProperties?.Headers?.TryGetValue("retry_count", out headerValue);
+        }
+        catch (Exception) { }
+
+        int retryCount = headerValue != null ? (int)headerValue : 0;
+
+        var message = Encoding.UTF8.GetString(eventArgs.Body);
+        ReceiveMessage?.Invoke(message, retryCount, eventArgs.DeliveryTag);
+    }
+
+    public void Dispose()
+    {
+        ChannelFactory.CloseConnection();
     }
 }
